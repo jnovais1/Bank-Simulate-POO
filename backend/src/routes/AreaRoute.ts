1de57f6ac7796970a937ef6b4c8dba1ed73e1d63
jnovais1 <jnovais@findes.org.br>
2022-08-05 16:22:48 -0300

    Criação rotas para área_video

--- backend/src/routes/AreaRoute.ts
+++ backend/src/routes/AreaRoute.ts
@@ -17,21 +17,11 @@ const createAreaController = new CreateAreaController();
 const updateAreaController = new UpdateAreaController();
 const readAreaController = new ReadAreaController();
 const readAllAreasController = new ReadAllAreasController();
-const createAreaVideoController = new CreateAreaVideoController();
-const readAreaVideoById = new ReadAreaVideoController();
-const deleteAreaVideoByID = new DeleteAreaVideoByIDController();
-const updateAreaVideo = new UpdateAreaVideoController();
 
 areaRoutes.get('/', readAllAreasController.handle);
 areaRoutes.get('/:id', readAreaController.handle);
 areaRoutes.post('/', ensureAuthenticateUser, createAreaController.handle);
 areaRoutes.put('/:id', ensureAuthenticateUser, updateAreaController.handle);
 areaRoutes.delete('/:id', ensureAuthenticateUser, deleteAreaByIDController.handle);
-
-areaRoutes.get('/video/:id', readAreaVideoById.handle);
-areaRoutes.post('/video', ensureAuthenticateUser, createAreaVideoController.handle);
-areaRoutes.put('/video/:id', ensureAuthenticateUser, updateAreaVideo.handle);
-areaRoutes.delete('/video/:id', ensureAuthenticateUser, deleteAreaVideoByID.handle);
-
 
 export {areaRoutes};
