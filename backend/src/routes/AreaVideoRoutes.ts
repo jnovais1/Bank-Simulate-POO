907cbde6408ae8c723a05bd61f9f417d88dfbdc6
jnovais1 <jnovais@findes.org.br>
2022-08-09 16:43:02 -0300

    Alteração rota

--- backend/src/routes/AreaVideoRoutes.ts
+++ backend/src/routes/AreaVideoRoutes.ts
@@ -1,23 +1,21 @@
 import { Router } from "express";
 import { ensureAuthenticateUser } from "../middlewares/ensureAuthenticateUser";
-import { CreateAreaVideoController } from "../Modules/Areas/create_area/area_video/CreateAreaVideo/CreateAreaVideoController";
-import { DeleteAreaVideoByIDController } from "../Modules/Areas/create_area/area_video/DeleteAreaVideo/DeleteAreaVideoController";
-import { ReadAreaVideoController } from "../Modules/Areas/create_area/area_video/ReadAreaVideo/ReadAreaVideoController";
-import { UpdateAreaVideoController } from "../Modules/Areas/create_area/area_video/UpdateArea/UpdateAreaVideoController";
+import { CreateAreaVideoController } from "../Modules/Areas/area_video/CreateAreaVideo/CreateAreaVideoController";
+import { DeleteAreaVideoByIDController } from "../Modules/Areas/area_video/DeleteAreaVideo/DeleteAreaVideoController";
+import { ReadAreaVideoController } from "../Modules/Areas/area_video/ReadAreaVideo/ReadAreaVideoController";
+import { UpdateAreaVideoController } from "../Modules/Areas/area_video/UpdateAreaVideo/UpdateAreaVideoController";
 import { CreateAreaController } from "../modules/Areas/create_area/CreateAreaController";
 
 const areaVideoRoutes = Router();
 
 const createAreaVideoController = new CreateAreaVideoController();
-const createArea = new CreateAreaController()
-
 const readAreaVideoById = new ReadAreaVideoController();
 const deleteAreaVideoByID = new DeleteAreaVideoByIDController();
 const updateAreaVideo = new UpdateAreaVideoController();
 
 areaVideoRoutes.get('/:id', readAreaVideoById.handle);
-areaVideoRoutes.post('/', ensureAuthenticateUser, createArea.handle);
-areaVideoRoutes.put('/:id', ensureAuthenticateUser, updateAreaVideo.handle);
+areaVideoRoutes.post('/', ensureAuthenticateUser, createAreaVideoController.handle);
+areaVideoRoutes.put('/:id/', ensureAuthenticateUser, updateAreaVideo.handle);
 areaVideoRoutes.delete('/:id', ensureAuthenticateUser, deleteAreaVideoByID.handle);
 
 export { areaVideoRoutes };
