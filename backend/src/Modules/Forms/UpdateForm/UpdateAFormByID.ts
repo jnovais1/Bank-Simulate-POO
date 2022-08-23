2a5d70d96b7fe15485d126e940e1e98ef83174b2
jnovais1 <jnovais@findes.org.br>
2022-08-23 16:13:21 -0300

    URL Validator

--- backend/src/Modules/Forms/UpdateForm/UpdateAFormByID.ts
+++ backend/src/Modules/Forms/UpdateForm/UpdateAFormByID.ts
@@ -1,4 +1,5 @@
 import { prisma } from "../../../database/PrismaClient"
+import { URLValidator } from "../../../services/Link-Validator";
 
 interface IUpdateForm {
     form_id: number,
@@ -7,17 +8,23 @@ interface IUpdateForm {
     link: string
 }
 export class UpdateForms {
-    async execute({title, description, form_id, link}: IUpdateForm) {
-        const result = await prisma.form.update({
-            where: {
-                id: form_id
-            },
-            data: {
-                title,
-                link,
-                description
-            },
-        });
-        return result;
+    async execute({ title, description, form_id, link }: IUpdateForm) {
+        const urlValidator = new URLValidator();
+        const linkValido = urlValidator.validateUrl(link);
+        if (linkValido) {
+            const result = await prisma.form.update({
+                where: {
+                    id: form_id
+                },
+                data: {
+                    title,
+                    link,
+                    description
+                },
+            });
+            return result;
+        }else {
+            throw new Error("Link inválido. ")
+        }
     }
 }
